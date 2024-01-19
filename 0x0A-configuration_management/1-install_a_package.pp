9454d61dbb7d8a7924d2eebbf7b9bbb0696b169e
ohendarko <ohendarko@gmail.com>
2024-01-19 14:09:30 +0000

    submit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 #  Install flask from pip3
 
+package{ 'python3-pip':
+  ensure => present,
+}
+
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
 }
