fdf4f37c45c005a79d7cccaf4312f6c99e46af48
ohendarko <ohendarko@gmail.com>
2024-01-21 15:36:52 +0000

    submit

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,20 +1,17 @@
 # Puppet manifest for SSH client configuration
 
-exec { 'turn-off-password-auth':
-  command     => '/bin/sed -i "s/^PasswordAuthentication.*/PasswordAuthentication no/" /etc/ssh/sshd_config',
-  refreshonly => true,
-}
-
-file { '/root/.ssh':
-  ensure  => directory,
-}
-
-file { '/root/.ssh/config':
+file { '/home/ubuntu/.ssh/config':
   ensure  => present,
-  content => "IdentityFile ~/.ssh/school\n",
-}
-
-exec { 'reload-ssh':
-  command     => '/bin/systemctl reload sshd',
-  refreshonly => true,
+  content => "\
+Host 460690-web-01
+    HostName 54.145.240.142
+    User ubuntu
+    IdentityFile ~/.ssh/school
+    PreferredAuthentications publickey
+    PasswordAuthentication no
+    PubkeyAuthentication yes
+",
+  owner   => ubuntu,
+  group   => ubuntu,
+  mode    => '0600',
 }
