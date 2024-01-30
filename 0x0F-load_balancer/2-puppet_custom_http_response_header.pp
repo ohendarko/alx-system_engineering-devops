e143e8f64402a5ba26aef5629de067e813784df2
ohendarko <ohendarko@gmail.com>
2024-01-30 02:11:54 +0000

    submit

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,26 @@
+# Install packages
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file { '/etc/nginx/sites-available/custom_header':
+  ensure  => present,
+  content => "server {
+                listen 80 default_server;
+                listen [::]:80 default_server;
+                add_header X-Served-By ${hostname};
+                }
+              }",
+}
+
+file { '/etc/nginx/sites-enabled/custom_header':
+  ensure => link,
+  target => '/etc/nginx/sites-available/custom_header',
+}
+
+service { 'nginx':
+  ensure    => running,
+  enable    => true,
+  subscribe => File['/etc/nginx/sites-available/custom_header'],
+}
