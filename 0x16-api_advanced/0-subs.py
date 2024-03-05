7fd9270392141fb0d501e03b4d0962f11d5f0f4e
ohendarko <ohendarko@gmail.com>
2024-03-05 23:35:13 +0000

    how many subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Contains a function that queries the Reddit API
+and returns the number of subscribers
+(not active users, total subscribers)
+for a given subreddit.
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    queries the Reddit API
+    and returns the number of subscribers
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Custom User-Agent'}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
