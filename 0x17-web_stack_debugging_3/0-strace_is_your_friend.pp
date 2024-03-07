a4dbaa46209b9912e95b7623a0da39254529a1a0
ohendarko <ohendarko@gmail.com>
2024-03-07 18:19:33 +0000

    phpp

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Looks like we had bad .phpp extensions instead of .php for wp-settings.php
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
